14cdbe24249e03dd5934819e4560c71dca03e21b
Jack-170 <agavejack@hotmail>
2023-11-23 17:35:12 +0100

    feat: previous & infinte loop

--- js/script.js
+++ js/script.js
@@ -5,30 +5,57 @@ console.log (items);
 
 let activeItem = 0;
 
-// nav
+// next 
 
 const next = document.querySelector(".next");
 
 next.addEventListener("click" ,
 
     function () {
-
         if (activeItem < items.length - 1) {
             // rimuovo active dall'elemento
             items[activeItem].classList.remove("active");
-
             // incremento indice elemento da visualizzare
-             activeItem++;
-
+            activeItem++;
             //  passo la classe active all'elemento successivo
-
             items[activeItem].classList.add("active");
-        } 
+        } else {
+            // "ciclo infinito"
+            items[activeItem].classList.remove("active");
+            activeItem = 0;
+            items[activeItem].classList.add("active");
+        }
 
-        if (activeItem === items.length - 1) {
+    }
 
-            next.classList.add("d-none");
 
+)
+
+
+// previous
+
+const previous = document.querySelector(".previous");
+
+previous.addEventListener("click" ,
+    function(){
+        // if (activeItem > 0) {
+        //     items[activeItem].classList.remove("active");
+        //     activeItem--;
+        //     items[activeItem].classList.add("active");
+        // }
+
+        if (activeItem > 0) {
+             // rimuovo active dall'elemento
+            items[activeItem].classList.remove("active");
+            // decremento indice elemento da visualizzare
+            activeItem--;
+             // passo la classe active all'elemento precedente
+            items[activeItem].classList.add("active");
+        } else {
+            // "loop infinito"
+            items[activeItem].classList.remove("active");
+            activeItem = items.length - 1;
+            items[activeItem].classList.add("active");
         }
     }
 
